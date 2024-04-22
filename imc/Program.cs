5f0e828992781e020be9e6608b3e52018352e7fa
AnnyaB <anabordin2008@gmail.com>
2024-04-22 10:30:31 -0300

    tabela diferenca sexo

--- imc/Program.cs
+++ imc/Program.cs
@@ -1,13 +1,24 @@
 ﻿// 6.faça um programa que leia o peso, altura e sexo e calcule o IMC informando a situação da pessoa;
 /*
 IMC = peso / altura^2
-Menor que 18,5 - Baixo peso
-De 18,5 a 24,9 - Peso normal
-De 25 a 29,9 - Sobrepeso
-De 30 a 34,9 - Obesidade grau I
-De 35 a 39.9 - Obesidade grau II
+
+mulher
+Menor que 19,1 - Baixo peso
+De 19,1 a 25,8 - Peso normal
+De 25,8 a 27,3 - Sobrepeso
+De 27,3 a 31,1 - Obesidade I
+De 31,1 a 40 - Obesidade II
+Igual ou maior que 40 - Obesidade grau III
+
+homem
+Menor que 20,7 - Baixo peso
+De 20,7 a 26,4 - Peso normal
+De 26,4 a 27,8 - Sobrepeso
+De 27,8 a 32,3 - Obesidade I
+De 32,3 a 40 - Obesidade II
 Igual ou maior que 40 - Obesidade grau III
 */
+
 int res = 0;
 double altura, peso, imc;
 string sexo;
@@ -21,7 +32,7 @@ do
     peso = 0;
     imc = 0;
     sexo = null;
-    valido = false; 
+    valido = false;
 
     while (altura <= 0 || altura > 2.60)
     {
@@ -37,41 +48,71 @@ do
 
     while (valido != true)
     {
-        Console.WriteLine("Informe seu sexo com letra minuscula:\nf - feminino;\nm - masculino; \no - outro;");
+        Console.WriteLine("Informe seu sexo com letra minuscula:\nf - feminino;\nm - masculino;");
         sexo = Console.ReadLine();
-        if (sexo == "f" || sexo == "m" || sexo == "o")
+        if (sexo == "f" || sexo == "m")
         {
             valido = true;
         }
     }
-
+    // 0.00 -> formatar
     imc = peso / Math.Pow(altura, 2);
-
-    if (imc <= 18.5)
+    if (sexo == "f")
     {
-        Console.WriteLine($"Seu IMC é de: {imc},\nvocê está abaixo do peso;");
+        if (imc <= 19.1)
+        {
+            Console.WriteLine($"Seu IMC é de: {imc},\nvocê está abaixo do peso;");
+        }
+        else if (imc > 19.1 && imc <= 25.8)
+        {
+            Console.WriteLine($"Seu IMC é de: {imc},\nvocê está no peso ideal;;");
+        }
+        else if (imc > 25.8 && imc <= 27.3)
+        {
+            Console.WriteLine($"Seu IMC é de: {imc},\nvocê está em sobrepeso;");
+        }
+        else if (imc > 27.3 && imc <= 31.1)
+        {
+            Console.WriteLine($"Seu IMC é de: {imc},\nvocê está com obesidade grau I;");
+        }
+        else if (imc > 31.1 && imc <= 40)
+        {
+            Console.WriteLine($"Seu IMC é de: {imc},\nvocê está com obesidade grau II;");
+        }
+        else
+        {
+            Console.WriteLine($"Seu IMC é de: {imc},\nvocê está com obesidade grau III;");
+        }
     }
-    else if (imc > 18.5 && imc <= 24.9)
+    if (sexo == "m")
     {
-        Console.WriteLine($"Seu IMC é de: {imc},\nvocê está no peso ideal;;");
-    }
-    else if (imc > 24.9 && imc <= 29.9)
-    {
-        Console.WriteLine($"Seu IMC é de: {imc},\nvocê está em sobrepeso;");
-    }
-    else if (imc > 29.9 && imc <= 34.9)
-    {
-        Console.WriteLine($"Seu IMC é de: {imc},\nvocê está com obesidade grau I;");
-    }
-    else if (imc > 34.9 && imc <= 39.9)
-    {
-        Console.WriteLine($"Seu IMC é de: {imc},\nvocê está com obesidade grau II;");
-    }
-    else
-    {
-        Console.WriteLine($"Seu IMC é de: {imc},\nvocê está com obesidade grau III;");
+        if (imc <= 20.7)
+        {
+            Console.WriteLine($"Seu IMC é de: {imc},\nvocê está abaixo do peso;");
+        }
+        else if (imc > 20.7 && imc <= 26.4)
+        {
+            Console.WriteLine($"Seu IMC é de: {imc},\nvocê está no peso ideal;;");
+        }
+        else if (imc > 26.4 && imc <= 27.8)
+        {
+            Console.WriteLine($"Seu IMC é de: {imc},\nvocê está em sobrepeso;");
+        }
+        else if (imc > 27.8 && imc <= 32.3)
+        {
+            Console.WriteLine($"Seu IMC é de: {imc},\nvocê está com obesidade grau I;");
+        }
+        else if (imc > 32.3 && imc <= 40)
+        {
+            Console.WriteLine($"Seu IMC é de: {imc},\nvocê está com obesidade grau II;");
+        }
+        else
+        {
+            Console.WriteLine($"Seu IMC é de: {imc},\nvocê está com obesidade grau III;");
+        }
     }
 
     Console.WriteLine("Deseja fazer outro IMC?\nPara continuar digite 0;\nPara parar digite qualquer outro número:");
     res = int.Parse(Console.ReadLine());
-} while (res == 0)
+
+} while (res == 0);
